619b9405e5765683a150f54c01af53af30a58838
Thang Phan Hoang <111140626+thangph1003@users.noreply.github.com>
2026-01-14 17:32:05 +0700

    feat : component ProjectZoning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Banner from "@/components/Banner";
 import DownloadSection from "@/components/DownloadPdf"
+import ProjectZoning from "@/components/ProjectZoning";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <Banner />
-      <p>Layout</p>
       <DownloadSection/>
+      <ProjectZoning />
     </div>
   );
 }
