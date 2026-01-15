cf172614ab7721ad40b68e3da8fb61d04801e137
Thang Phan Hoang <111140626+thangph1003@users.noreply.github.com>
2026-01-15 09:15:23 +0700

    feat : component TravelCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Banner from "@/components/Banner";
 import DownloadSection from "@/components/DownloadPdf"
 import ProjectZoning from "@/components/ProjectZoning";
+import TravelCards from "@/components/TravelCard";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <Banner />
       <DownloadSection/>
+      <TravelCards />
       <ProjectZoning />
     </div>
   );
